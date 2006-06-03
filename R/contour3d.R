d23a537c2c96d91dcaba7a20f8d65597e6ea0799
Luke Tierney <luke-tierney@uiowa.edu>
2006-06-03 22:49:25 +0000

    Separated rendering into drawScene.rgl.

--- R/contour3d.R
+++ R/contour3d.R
@@ -641,42 +641,8 @@ contour3d <- function(f, level,
         scene
     else {
         scene <- colorScene(scene)
-        if (engine == "rgl") {
-            if (! rgl.cur())
-                open3d()
-            if (!add)
-                rgl.clear()
-
-            triangles <- canonicalizeAndMergeScene(scene, "color", "alpha",
-                                                   "col.mesh", "fill")
-            col <- rep(triangles$color, each = 3)
-            alpha <- rep(triangles$alpha, each = 3)
-            fill <- rep(triangles$fill, each = 3)
-            col.mesh <- rep(triangles$col.mesh, each = 3)
-            data <- zipTriangles(triangles)
-            if (all(fill)) {
-                front <- "filled"
-                back <- "filled"
-            }
-            else if (any(fill))
-                ##**** handle these by splitting; OK if no alpha < 1
-                stop(paste("for now rgl engine cannot handle mixed fill/wire",
-                           "frame contours"))
-            else {
-                front <- "lines"
-                back <- "lines"
-                col <- col.mesh
-            }
-            oldstyle = FALSE #*** eventually make this a settable option.
-            if (oldstyle) {
-                data <- data[,c(1, 3, 2)]
-                data[,3] <- -data[,3]
-            }
-            if (nrow(data) > 0) # to avoid a segfault in rgl
-                rgl.triangles(data[,1], data[,2], data[,3],
-                              col = col, alpha = alpha,
-                              front = front, back = back, ...)
-        }
+        if (engine == "rgl")
+            drawScene.rgl(scene, add = add, ...)
         else stop(paste("unknown rendering engine:", engine))
     }
 }
