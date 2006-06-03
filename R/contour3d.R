055127606f50b6130ba8209ed5091bfa19a198c4
Luke Tierney <luke-tierney@uiowa.edu>
2006-06-03 19:36:18 +0000

    Minor rearranging in contour3d.

--- R/contour3d.R
+++ R/contour3d.R
@@ -642,37 +642,37 @@ contour3d <- function(f, level,
     else {
         scene <- colorScene(scene)
         if (engine == "rgl") {
+            if (! rgl.cur())
+                open3d()
+            if (!add)
+                rgl.clear()
+
             triangles <- canonicalizeAndMergeScene(scene, "color", "alpha",
                                                    "col.mesh", "fill")
-            triangles$color <- rep(triangles$color, each = 3)
-            triangles$alpha <- rep(triangles$alpha, each = 3)
-            triangles$fill <- rep(triangles$fill, each = 3)
-            triangles$col.mesh <- rep(triangles$col.mesh, each = 3)
+            col <- rep(triangles$color, each = 3)
+            alpha <- rep(triangles$alpha, each = 3)
+            fill <- rep(triangles$fill, each = 3)
+            col.mesh <- rep(triangles$col.mesh, each = 3)
             data <- zipTriangles(triangles)
-            col <- triangles$color
-            alpha <- triangles$alpha
-            if (all(triangles$fill)) {
+            if (all(fill)) {
                 front <- "filled"
                 back <- "filled"
             }
-            else if (any(triangles$fill))
+            else if (any(fill))
                 ##**** handle these by splitting; OK if no alpha < 1
                 stop(paste("for now rgl engine cannot handle mixed fill/wire",
                            "frame contours"))
             else {
                 front <- "lines"
                 back <- "lines"
+                col <- col.mesh
             }
             oldstyle = FALSE #*** eventually make this a settable option.
             if (oldstyle) {
                 data <- data[,c(1, 3, 2)]
                 data[,3] <- -data[,3]
             }
-            if (! rgl.cur())
-                open3d()
-            if (!add)
-                rgl.clear()
-            if (nrow(data) > 0)
+            if (nrow(data) > 0) # to avoid a segfault in rgl
                 rgl.triangles(data[,1], data[,2], data[,3],
                               col = col, alpha = alpha,
                               front = front, back = back, ...)
